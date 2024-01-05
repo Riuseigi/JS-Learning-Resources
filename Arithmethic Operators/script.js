30393dd638ea3bc001b5990d3936ae5fa1a5206e
Riuseigi <troyliamjarata@gmail.com>
2024-01-05 09:23:03 +0800

    learn arithmetic operators

---
+++ Arithmethic Operators/script.js
@@ -0,0 +1,39 @@
+// arithmetic operators = opperands (values, variables, etc.)
+//                      = operators (+ = * /)
+//                      ex. 11 = x +5
+
+
+
+let students =30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+
+// students = students **3 // power of
+// let extraStudents = students% 3; Modolus
+
+//Augmented Assignment
+// students += 1
+// students -= 1
+// students *= 2
+// students /= 2
+// students **= 2
+// students %= 2
+
+//incrementation
+// students++
+// students--
+
+/*
+operator precedence
+    1.Parenthesis()
+    2.exponent
+    3. Multiplication & division & modulo
+    4. addition & substraction
+*/
+
+let resukt = 1+ 2 * 3 + 4 ** 2;
+
+console.log(resukt)
