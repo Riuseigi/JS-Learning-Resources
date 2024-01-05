09afeb258215dfd1e9da6f02f06ca584e3bb8e26
Riuseigi <troyliamjarata@gmail.com>
2024-01-05 08:52:00 +0800

    learn variables

---
+++ Variables/script.js
@@ -0,0 +1,46 @@
+// variable = a container that stores a value.
+// behaves as if it were the value it contains.
+
+
+// 1.declaration         let x;
+// 2. assignment         x = 100;
+
+// let age = 25;
+
+// let price = 10.99
+
+// let gpa = 2.1
+
+// let firstName = "Troy"
+// let favoriteFood = "sushi"
+// let email = "troyliamjarata@gmail.com"
+
+// console.log(typeof firstName)
+// console.log(`Your name is ${firstName}`)
+// console.log(`You like ${favoriteFood}`)
+// console.log(`Your email is ${email}`)
+
+// console.log(`You are ${age} years old\nThe price is ${price}`)
+// console.log(`Your gpa is: ${gpa}`)
+
+
+
+// let online = true;
+// let forSale = false
+// let isStudent = true
+
+// console.log(`Bro is online: ${online}`)
+// console.log(`Is this car for sale: ${forSale}`)
+// console.log(`Enrolled: ${isStudent}`)
+
+
+let fullName ="Troy Liam Jarata"
+let age = 23
+let student = false 
+
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`
+document.getElementById("p2").textContent = `Your are ${age} years old`
+document.getElementById("p3").textContent = `Enrolled: ${student}`
+
+
