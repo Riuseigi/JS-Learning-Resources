edba9ab30a08fe0f975569720764032f325c017c
Riuseigi <troyliamjarata@gmail.com>
2024-01-08 10:28:21 +0800

    learn Strictly operator

---
+++ Strict Equality/script.js
@@ -0,0 +1,19 @@
+//  = assignment operator
+// == comparison operator (compare if values are equal)
+// === strict equality operator(compare if values & datatyoe are equal)
+// != inequality operator
+// !== strict inequality operator
+
+
+const PI = 3.14;
+// Doesnt care the the data type
+// PI=="3.14"? console.log("This is a PI"): console.log("This is NOT a PI")
+
+
+//Strictly
+// PI==="3.14"? console.log("This is a PI"): console.log("This is NOT a PI")
+
+
+// PI!="3.14"? console.log("This is not a PI"): console.log("This is a PI")
+
+PI!=="3.14"? console.log("This is not a PI"): console.log("This is a PI")
