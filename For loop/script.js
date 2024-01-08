1e0568be7c28a3509dbf9d059487eae2024a674c
Riuseigi <troyliamjarata@gmail.com>
2024-01-08 11:09:48 +0800

    learn for loops

---
+++ For loop/script.js
@@ -0,0 +1,27 @@
+//for loop = repeat some code a limited amount of times
+
+
+// for(Initialiazation;condtion;iteration)
+// for(let i = 10;i>0; i--){
+//     console.log(i)
+// }
+
+// console.log("Happy New Year")
+
+
+for(let i=0; i <=20; i++){
+   
+    // if(i==13){
+    //     continue;
+    // }
+    // else{
+    //     console.log(i)
+    // }
+
+    if(i==13){
+        break;
+    }
+    else{
+        console.log(i)
+    }
+}
