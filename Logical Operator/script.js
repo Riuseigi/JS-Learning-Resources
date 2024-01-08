3f631bbd5e02fa308a79070d472fffa4a92e7fb8
Riuseigi <troyliamjarata@gmail.com>
2024-01-08 10:15:26 +0800

    learn logical operator

---
+++ Logical Operator/script.js
@@ -0,0 +1,30 @@
+// Lofical Operators = used to combine or manipulate bollean values (true of False)
+// AND = &&
+// OR = ||
+// NOT = !
+
+
+const temp = -10;
+
+//AND Method
+// if(temp>0 && temp<=30){
+//     console.log("The Weather is Good")
+// }
+// else{
+//     console.log("The Weather is bad")
+// }
+
+
+//OR Method
+// if(temp<=0 || temp>=30){
+//     console.log("The Weather is Bad")
+// }
+// else{
+//     console.log("The Weather is Good")
+// }
+
+
+//Not Method
+let isSunny = false
+
+ !isSunny ?  console.log("it is Cloudy"): console.log("it is Sunny")
