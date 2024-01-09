a31d44ad99178718516fa7b1edcc7af106ec071c
Riuseigi <troyliamjarata@gmail.com>
2024-01-09 19:24:01 +0800

    learn arrays

---
+++ Arrays/script.js
@@ -0,0 +1,60 @@
+// array = a variable like structure that can hold more than 1 value
+
+// set the contain of index
+let fruits = ['apple','orange', "banana", "coconut"];
+
+// add element to last
+// fruits.push('coconut')
+
+
+//remove the last element
+// fruits.pop();
+
+
+//add element to the first index
+// fruits.unshift("mango")
+
+//remove the first element
+// fruits.shift()
+
+// change the element
+// fruits[2] = "coconut"
+
+
+//access all
+// console.log(fruits)
+
+//access specific index []
+// console.log(fruits[2])
+
+
+
+// identify the length of array
+// let numOfFruits = fruits.length
+// console.log(numOfFruits)
+
+
+//find the index of element in array
+// let index = fruits.indexOf('apple')
+
+//list all the elements
+// for(let i = 0;i <fruits.length;i++){
+//     console.log(fruits[i])
+// }
+
+//sort the array
+fruits.sort().reverse()
+
+// shorthand
+for(let fruit of fruits){
+    console.log(fruit)
+}
+
+console.log(index)
+
+
+
+
+
+
+
