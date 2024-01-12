b6bd5baf12ea30d15a0e63e0d5758775589839cd
Riuseigi <troyliamjarata@gmail.com>
2024-01-12 18:52:38 +0800

    learn function expression

---
+++ Function Expression/script.js
@@ -0,0 +1,53 @@
+// function declaration = define a reusable block of code that performs a specific task
+
+// function hello(){
+//     console.log("Hello")
+// }
+
+// function expressions = a way to define functions as values or variables
+
+// const hello = function(){
+//     console.log("Hello")
+// }
+
+// hello()
+
+
+
+// example 1
+
+// setTimeout(function(){
+//     console.log("hello")
+// }, 3000)
+
+// example 2
+
+// const numbers = [1,2,3,4,5,6,8,9,10]
+
+// const squares= numbers.map(function(element){
+//     return Math.pow(element,2)
+// })
+
+
+// const cubes = numbers.map(function(element){
+//     return Math.pow(element,4)
+// })
+
+
+
+// const isEven = numbers.filter(function(element){
+//     return element %2 ===0 ? element : null
+// })
+
+// const isOdd = numbers.filter(function(element){
+//     return element %2 !==0 ? element : null
+// })
+
+
+// const total = numbers.reduce(function(accumulator,element){
+//     return  accumulator+element
+// })
+// console.log(total)
+
+
+
