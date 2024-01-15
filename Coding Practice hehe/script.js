1c4a6b6c8131cc0d9f628782bf550b68ac2240ad
Riuseigi <troyliamjarata@gmail.com>
2024-01-15 11:38:49 +0800

    solve coding practice 2

--- Coding Practice hehe/script.js
+++ Coding Practice hehe/script.js
@@ -5,55 +5,33 @@
 // Explore the this keyword within methods.
 
 
-class Calculator {
-  constructor(x,y){
-    this.x = x
-    this.y = y
-  }
-  add(){
-    console.log(`The Result is : ${this.x+this.y}`)
-  }
-  substract(){
-    console.log(`The Result is : ${this.x-this.y}`)
-  }
-  multiply(){
-    console.log(`The Result is : ${this.x*this.y}`)
-  }
-  divide(){
-    console.log(`The Result is : ${this.x/this.y}`)
-  }
-}
+// class Calculator {
+//   constructor(x,y){
+//     this.x = x
+//     this.y = y
+//   }
+//   add(){
+//     console.log(`The Result is : ${this.x+this.y}`)
+//   }
+//   substract(){
+//     console.log(`The Result is : ${this.x-this.y}`)
+//   }
+//   multiply(){
+//     console.log(`The Result is : ${this.x*this.y}`)
+//   }
+//   divide(){
+//     console.log(`The Result is : ${this.x/this.y}`)
+//   }
+// }
 
-const resultAdd = new Calculator(3,2)
-const resultSubstract = new Calculator(3,2)
-const resultMultiply = new Calculator(3,2)
-const resultDivide = new Calculator(3,2)
-resultAdd.add()
-resultSubstract.substract()
-resultMultiply.multiply()
-resultDivide.divide()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const resultAdd = new Calculator(3,2)
+// const resultSubstract = new Calculator(3,2)
+// const resultMultiply = new Calculator(3,2)
+// const resultDivide = new Calculator(3,2)
+// resultAdd.add()
+// resultSubstract.substract()
+// resultMultiply.multiply()
+// resultDivide.divide()
 
 
 
@@ -63,6 +41,62 @@ resultDivide.divide()
 // Design a BankAccount class with properties like balance, accountNumber, and ownerName.
 // Implement methods for depositing, withdrawing, and transferring funds.
 // Handle errors gracefully (e.g., insufficient funds).
+
+
+class BankAccount{
+
+  static balance =0;
+  constructor(ownerName,initialBalance,accountNumber){
+    this.initialBalance = initialBalance;
+    this.ownerName = ownerName;
+    this.accountNumber =ownerName
+    BankAccount.balance+=initialBalance
+  }
+  deposit(amount){
+    return BankAccount.balance+= amount
+  }
+  checkBalance(){
+    console.log(`Hi ${this.ownerName}, Your Balance is: ${BankAccount.balance}`)
+  }
+  withdraw(amount){
+    if(BankAccount.balance<amount){
+      console.log(`Sorry ${this.ownerName}, you are insufficient funds`)
+    }
+    else{
+      return BankAccount.balance-=amount
+    }
+  }
+  transfer(amount,transferName){
+    if(BankAccount.balance<amount){
+      console.log(`Sorry ${this.ownerName}, you are insufficient funds`)
+    }
+    else{
+      BankAccount.balance-=amount
+      console.log(`You are transferred funds to ${transferName} with ${amount}`)
+    }
+  }
+
+}
+
+
+const troy = new BankAccount('Troy',0,1234567)
+troy.deposit(100)
+troy.withdraw(50)
+troy.checkBalance()
+troy.transfer(20,"Diana")
+troy.checkBalance()
+
+
+
+
+
+
+
+
+
+
+
+
 // 3. Model a Vehicle Inventory System:
 
 // Create a Vehicle class with properties like make, model, year, and price.
