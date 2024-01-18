db6f2cb8b21855b3789a59b9d8030d0408e751c2
Riuseigi <troyliamjarata@gmail.com>
2024-01-18 15:19:32 +0800

    solve mahine problem

--- Getter and Setter/script.js
+++ Getter and Setter/script.js
@@ -43,47 +43,101 @@
 
 //Example 2
 
-class Person{
-  constructor(firstName,lastName,age){
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
+// class Person{
+//   constructor(firstName,lastName,age){
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.age = age
+//   }
+//   set firstName(newFirstName){
+//       if(typeof newFirstName === "string" && newFirstName.length > 0){
+//         return this._firstName = newFirstName
+//       }else{
+//         console.error(`The First Name should be a String and greater than 0`)
+//       }
+//   }
+//   set lastName(newLastName){
+//     if(typeof newLastName === "string" && newLastName.length > 0){
+//        this._lastName = newLastName
+//     }else{
+//       console.error(`The Last Name should be a String and greater than 0`)
+//     }
+//   }
+//   set age(newAge){
+//     if(typeof newAge === "number" && newAge > 0){
+//       this._age = newAge
+//     }else{
+//       console.error(`The age should be a number and greater than 0`)
+//     }
+//   }
+
+//   get firstName(){
+//     return this._firstName
+//   }
+//   get lastName(){
+//     return this._lastName
+//   }
+//   get age(){
+//     return this._age
+//   }
+
+// }
+
+// const person1 = new Person("Diana","Palaganas",22)
+// person1.age = "n"
+
+// console.log(person1.age)
+
+
+//Machine Problem 1
+
+// Temperature Conversion:
+
+// Create a Temperature object with celsius and fahrenheit properties.
+// Use getters and setters to ensure that:
+// Setting one property automatically updates the other.
+// The temperature cannot be set below absolute zero (-273.15°C or -459.67°F).
+
+
+class Temperature{
+  constructor(celcious,farenheit){
+    this.celcious = celcious
+    this.farenheit = farenheit
   }
-  set firstName(newFirstName){
-      if(typeof newFirstName === "string" && newFirstName.length > 0){
-        return this._firstName = newFirstName
-      }else{
-        console.error(`The First Name should be a String and greater than 0`)
-      }
-  }
-  set lastName(newLastName){
-    if(typeof newLastName === "string" && newLastName.length > 0){
-       this._lastName = newLastName
-    }else{
-      console.error(`The Last Name should be a String and greater than 0`)
+  set celcious(newCelcious){
+    if(newCelcious >-273.15 ){
+      this._celcious = newCelcious
+    }
+    else{
+      console.log(`The temperature cannot be set below absolute zero`)
     }
   }
-  set age(newAge){
-    if(typeof newAge === "number" && newAge > 0){
-      this._age = newAge
-    }else{
-      console.error(`The age should be a number and greater than 0`)
+  set farenheit(newFarenheit){
+    if(newFarenheit >-273.15 ){
+      this._fareheit = newFarenheit
+    }
+    else{
+      console.log(`The temperature cannot be set below absolute zero`)
     }
   }
-
-  get firstName(){
-    return this._firstName
+  get celcious(){
+    return this._celcious
   }
-  get lastName(){
-    return this._lastName
+  get farenheit(){
+    return this._fareheit
   }
-  get age(){
-    return this._age
+  get celToFa(){
+    let result = this._celcious *(9/5) +32
+    console.log(`The Result is ${result}`)
   }
-
+  get faToCel(){
+    let result = (this._fareheit -32) * (5/9) 
+    console.log(`The Result is ${result.toFixed(2)}°F`)
+  }
 }
 
-const person1 = new Person("Diana","Palaganas",22)
-person1.age = "n"
+const celcios1 = new Temperature(35,154)
+celcios1.farenheit = 157
+console.log(celcios1.farenheit)
 
-console.log(person1.age)
+celcios1.faToCel
