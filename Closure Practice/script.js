a7695a1c8b102c57775f2fcaa6d1c036c7042264
Riuseigi <troyliamjarata@gmail.com>
2024-01-22 10:53:19 +0800

    coding practice 2 done

--- Closure Practice/script.js
+++ Closure Practice/script.js
@@ -4,20 +4,46 @@
 // Coding practice 1
 // Implementing a basic module pattern: Task: Create a module named MathUtils that contains two functions: add and multiply. These functions should only be accessible through the MathUtils module, not directly in the global scope.
 
-function MathUtils(){
-    function add(x,y){
-      return x+y
-    }
-    function multiply(x,y){
-      return x*y
-    }
-    return{add, multiply}
+// function MathUtils(){
+//     function add(x,y){
+//       return x+y
+//     }
+//     function multiply(x,y){
+//       return x*y
+//     }
+//     return{add, multiply}
+// }
+
+
+// const value = MathUtils()
+
+// console.log(value.add(1,2))
+
+
+
+// Coding practice 2  Implementing a basic module pattern:
+// Task: Create a class BankAccount with a deposit method that increases the balance, a withdraw method that decreases the balance, and a getBalance method that returns the current balance. However, the balance property itself should not be directly accessible outside the class.
+
+function BankAccount(){
+  let balance = 0;
+
+  function deposit(amount){
+    return balance+=amount;
+  }
+  function widthdraw(amount){
+    return balance-=amount;
+  }
+  function getBalance(){
+    console.log(`Your current balance is ${balance}`);
+  }
+  return{deposit,widthdraw,getBalance};
 }
 
 
-const value = MathUtils()
+const person1 = BankAccount();
 
-console.log(value.add(1,2))
+person1.deposit(100)
+person1.getBalance()
 
 
 
