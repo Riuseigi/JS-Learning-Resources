152fc87b8657e47e8ed5ff796afbb0b4f7367471
Riuseigi <troyliamjarata@gmail.com>
2024-01-30 22:30:58 +0800

    Learn Async and Await

---
+++ AsyncWait/script.js
@@ -0,0 +1,77 @@
+// Aync/Await - Async = makes a function return a promise
+//              Await = makes an async function wait for a promise
+
+
+// Allows you write asynchronous code in a synchronous manner
+// Async doesnt have resolve or reject parameters
+// Everything after Await is placed in an event queue
+function walkDog(){
+  
+  
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      const dogwalked =false;
+      if(dogwalked){
+        resolve("Walk the Dog 🐕")
+      }else{
+        reject("You didnt walk the Dog")
+      }
+      
+    },5000)
+  })
+}
+function cleanKitchen(){
+  
+  
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      const kitchenClean = true;
+      if(kitchenClean){
+        resolve("Clean the Kitchen 🧽");
+      }
+      else{
+        reject("You didnt clean the kitchen");
+      }
+      
+    },5000)
+  })
+}
+function takeOutTrash(){
+  
+  
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      const trashTakeOut = true;
+      if(trashTakeOut){
+        resolve("Take out the Trash");
+      }else{
+        reject("You didn't take out Trash");
+      }
+      
+    },200)
+  })
+}
+
+async function doChores(){
+
+  try{
+    const walkDogResult = await walkDog();
+
+    console.log(walkDogResult);
+  
+    const cleanKitchenResult = await cleanKitchen();
+    console.log(cleanKitchenResult);
+  
+    const takeOutTrashResult = await takeOutTrash();
+    console.log(takeOutTrashResult);
+  
+    console.log("Finish all the chores")
+  }
+  catch(error){
+    console.error(error)
+  }
+ 
+}
+
+        
+doChores()
