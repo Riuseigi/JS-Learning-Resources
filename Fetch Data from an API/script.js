8731c7db2a851c96e72e3d42081c5c4b30623754
Riuseigi <troyliamjarata@gmail.com>
2024-02-06 15:44:40 +0800

    add comments

--- Fetch Data from an API/script.js
+++ Fetch Data from an API/script.js
@@ -13,15 +13,19 @@
 //     })
 //     .then(data => console.log(data))
 //     .catch(error => console.error(error))
+
+//Initialization
 const pokemonNameText =document.getElementById("pokemonNameText")
 const pokemonTypeText = document.getElementById("pokemonType")
 const imgElement = document.getElementById("pokemonSprite")
 const pokemonIDText = document.getElementById("pokemonID")
 
+//Async Function
 async function fetchData(){
     
-    
+    //Try and Catch
     try{
+
         const pokemonNameInput = document.getElementById("pokemonName").value.toLowerCase();
     
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNameInput}`)
@@ -29,20 +33,27 @@ async function fetchData(){
         if(!response.ok){
             throw new Error ("Could not fetch Data")
         }
+        // Pokemon Data
         const data = await response.json();
-        console.log(data)
+        
+        //Pokemon Types and set to HTML
         const pokemonTypes = data.types.map(typeObject => typeObject.type.name);
-        pokemonTypeText.textContent = `Type: ${pokemonTypes}`
+        pokemonTypeText.textContent = `Type: ${pokemonTypes}`;
+        //Pokemon Name and set to HTML
         const pokemonName = data.name.toUpperCase();
         pokemonNameText.textContent = `${pokemonName}`
+
+        //Pokemon Sprite and set to HTML
        const pokemonSprite = data.sprites.front_default;
        imgElement.src = pokemonSprite;
        imgElement.style.display = "block"
+       //Pokemon ID and set to HTML
        const pokemonID = data.id;
        pokemonIDText.textContent = `ID: ${pokemonID}`
 
     }
     catch(error){
+        //Send Error and set to HTML
         pokemonNameText.textContent = error
         
     }
