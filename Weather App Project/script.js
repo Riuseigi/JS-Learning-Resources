6d2e8a7f224195fa9c83b49145d65d5c999d1a6e
Riuseigi <troyliamjarata@gmail.com>
2024-02-08 20:20:01 +0800

    add comments and clean code

--- Weather App Project/script.js
+++ Weather App Project/script.js
@@ -1,10 +1,17 @@
 // Weather app
 
+// Initialize the DOM Elements
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
+
+//API key fro weatherapi
 const apiKey = "fcc13824fac7c3854fa83934059d3ea4";
 
+
+/* This code is adding an event listener to the weatherForm element for the "submit" event. When the
+form is submitted, it prevents the default form submission behavior (which would refresh the page)
+using `event.preventDefault()`. */
 weatherForm.addEventListener("submit",async (event) => {
     event.preventDefault();
     const city = cityInput.value;
@@ -22,6 +29,14 @@ weatherForm.addEventListener("submit",async (event) => {
     }
 });
 
+
+/**
+ * The function `getWeatherData` is an asynchronous function that fetches weather data for a given city
+ * using the OpenWeatherMap API.
+ * @param city - The `city` parameter is a string that represents the name of the city for which you
+ * want to fetch weather data.
+ * @returns a promise that resolves to the weather data for the specified city.
+ */
 async function getWeatherData(city){
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
     const response = await fetch(apiUrl)
@@ -32,6 +47,8 @@ async function getWeatherData(city){
     return await response.json();
     
 }
+
+//function that display all the weather details of a specific city that enters by a user
 function displayWeatherInfo(data){
         console.log(data)
 
@@ -77,29 +94,10 @@ function displayWeatherInfo(data){
 
 
 }
+
+// Validate the weatherID to convert into emoji
 function getWeatherEmoji(weatherID){
-    // if(weatherID===800){
-    //     return `🌤️`
-        
-    // }
-    // else if(weatherID>=200 && weatherID<=232){
-    //     return `🌩️`
-    // }
-    // else if(weatherID>=300 && weatherID<=321){
-    //     return `🌩️`
-    // }
-    // else if(weatherID>=500 && weatherID<=531){
-    //     return `🌧️`
-    // }
-    // else if(weatherID>=600 && weatherID<=622){
-    //     return `🌨️`
-    // }
-    // else if(weatherID>=701 && weatherID<=781){
-    //     return `🌫️`
-    // }
-    // else{
-    //     return `☁️`
-    // }
+
     switch (true) {
    
         case (weatherID >= 200 && weatherID <= 232):
@@ -122,6 +120,8 @@ function getWeatherEmoji(weatherID){
       }
 
 }
+
+//function that display error
 function displayError(message){
    
     const errorDisplay = document.createElement("p")
