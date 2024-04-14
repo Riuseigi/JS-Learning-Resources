40c780dd65b234d7152bf5201922d9ec388d5655
Riuseigi <troyliamjarata@gmail.com>
2024-04-14 22:09:52 +0800

    slove machine problem 1

--- Array.every()/script.js
+++ Array.every()/script.js
@@ -7,3 +7,19 @@ const array1 = [1, 30, 39, 29, 10, 13];
 console.log(array1.every(isBelowThreshold));
 // Expected output: true
 
+//Problem 1: You are given an array of numbers. Write a function that takes the array and a target number, and returns true if every number in the array is greater than the target number, otherwise false.
+
+const numbers = [10, 20, 30, 40, 50];
+
+function isEveryNumberGreaterThanTarget(arr,target){
+     return arr.every((num) => {
+      return num > target;
+    })
+}
+
+
+console.log(isEveryNumberGreaterThanTarget(numbers, 5)); // Output: true
+console.log(isEveryNumberGreaterThanTarget(numbers, 25)); // Output: false
+
+
+
